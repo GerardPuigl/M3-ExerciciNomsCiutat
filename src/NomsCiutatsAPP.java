f5fc5c18793b9e6f0d34d8b843c4eae9695648ac
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-28 21:09:18 +0100

    Fase 1 Completada amb variables simples

--- src/NomsCiutatsAPP.java
+++ src/NomsCiutatsAPP.java
@@ -3,6 +3,32 @@ import java.util.*;
 public class NomsCiutatsAPP {
 	public static void main(String[] arg) {
 
+		String ciutat1;
+		String ciutat2;
+		String ciutat3;
+		String ciutat4;
+		String ciutat5;
+		String ciutat6;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introdueix el nom de 6 ciutats:");
+		
+		ciutat1 = sc.nextLine();
+		ciutat2 = sc.nextLine();
+		ciutat3 = sc.nextLine();
+		ciutat4 = sc.nextLine();
+		ciutat5 = sc.nextLine();
+		ciutat6 = sc.nextLine();
+		
+		System.out.println(ciutat1);
+		System.out.println(ciutat2);
+		System.out.println(ciutat3);
+		System.out.println(ciutat4);
+		System.out.println(ciutat5);
+		System.out.println(ciutat6);
+		
+		/* A continuación como creo que se debería hacer si el ejercicio no dijera especificamente utilizar variables String en vez de Array.
+		
 		// Crear 6 variables String y pedir por consolsa introducir nombres
 
 		String[] ciutat = new String[6];
@@ -18,6 +44,8 @@ public class NomsCiutatsAPP {
 		for (String c:ciutat) {
 			System.out.println(c);
 		}
+		*/
+		
 	}
 
 }
