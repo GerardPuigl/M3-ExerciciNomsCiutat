32d803ddfac8e157ecb1a2cfce299a1b30c45287
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-29 03:10:28 +0100

    Update Fase 1

--- src/NomsCiutatsAPP.java
+++ src/NomsCiutatsAPP.java
@@ -19,32 +19,6 @@ public class NomsCiutatsAPP {
 			System.out.println(c);
 		}
 		
-		/* Por si el ejercicio se debía hacer sin usar Array de variables.
-		
-		String ciutat1;
-		String ciutat2;
-		String ciutat3;
-		String ciutat4;
-		String ciutat5;
-		String ciutat6;
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introdueix el nom de 6 ciutats:");
-		
-		ciutat1 = sc.nextLine();
-		ciutat2 = sc.nextLine();
-		ciutat3 = sc.nextLine();
-		ciutat4 = sc.nextLine();
-		ciutat5 = sc.nextLine();
-		ciutat6 = sc.nextLine();
-		
-		System.out.println(ciutat1);
-		System.out.println(ciutat2);
-		System.out.println(ciutat3);
-		System.out.println(ciutat4);
-		System.out.println(ciutat5);
-		System.out.println(ciutat6);
-		*/
 		
 	}
 
