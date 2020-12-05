2292f7055bf3737c0a044d187df7e25b5661a2cf
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-12-05 18:37:30 +0100

    Fase 2 JS Completada

---
+++ JavaScript/NomCIutatsAPP.js
@@ -0,0 +1,14 @@
+// Crear 6 variables String y pedir por consolsa introducir nombres ciudades
+
+var ciutats = new Array;
+
+
+for (var i = 1; i < 7; i++) {   
+    ciutats.push(prompt("Intrdueix el nom la ciutat nº "+i))
+}
+
+//Ordenar Array
+
+ciutats.sort();
+
+ciutats.forEach(c => {    document.writeln(c,"<br/>");})
