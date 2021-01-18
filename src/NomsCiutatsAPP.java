883872343db2c67f1efb072a82a49bcbe887a52c
gerardpuig <gerardpuig@192.168.1.131>
2021-01-18 15:50:15 +0100

    Fase1 Completada

--- src/NomsCiutatsAPP.java
+++ src/NomsCiutatsAPP.java
@@ -3,23 +3,27 @@ import java.util.*;
 public class NomsCiutatsAPP {
 	public static void main(String[] arg) {
 
+		Scanner sc = new Scanner(System.in);
+
 		// Crear 6 variables String y pedir por consolsa introducir nombres ciudades
+		System.out.println("Introdueix el nom de 6 ciutats:");
+
+		String ciutat1 = sc.next();
+		String ciutat2 = sc.next();
+		String ciutat3 = sc.next();
+		String ciutat4 = sc.next();
+		String ciutat5 = sc.next();
+		String ciutat6 = sc.next();
 
 		String[] ciutat = new String[6];
 
-		System.out.println("Introdueix el nom de 6 ciutats:");
-		
-		for (int i = 0; i < 6; i++) {
-			 
-			Scanner sc = new Scanner(System.in);
-			ciutat[i] = sc.next();
-		}
-		
-		for (String c:ciutat) {
-			System.out.println(c);
-		}
-		
-		
+		System.out.println(ciutat1);
+		System.out.println(ciutat2);
+		System.out.println(ciutat3);
+		System.out.println(ciutat4);
+		System.out.println(ciutat5);
+		System.out.println(ciutat6);
+
 	}
 
 }
