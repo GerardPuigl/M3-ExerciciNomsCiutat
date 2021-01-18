7ef18a9d22fdaa21198eb20a543406252625f293
gerardpuig <gerardpuig@192.168.1.131>
2021-01-18 15:53:33 +0100

    Fase2 Completada

--- src/NomsCiutatsAPP.java
+++ src/NomsCiutatsAPP.java
@@ -3,16 +3,27 @@ import java.util.*;
 public class NomsCiutatsAPP {
 	public static void main(String[] arg) {
 
-		// Crear 6 variables String y pedir por consolsa introducir nombres
+		Scanner sc = new Scanner(System.in);
 
-		String[] ciutat = new String[6];
+		// Crear 6 variables String y pedir por consolsa introducir nombres ciudades
 		System.out.println("Introdueix el nom de 6 ciutats:");
 
-		for (int i = 0; i < 6; i++) {
-			Scanner sc = new Scanner(System.in);
-			ciutat[i] = sc.next();
-		}
+		String ciutat1 = sc.next();
+		String ciutat2 = sc.next();
+		String ciutat3 = sc.next();
+		String ciutat4 = sc.next();
+		String ciutat5 = sc.next();
+		String ciutat6 = sc.next();
 
+		String[] ciutat = new String[6];
+
+		ciutat[0]=ciutat1;
+		ciutat[1]=ciutat2;
+		ciutat[2]=ciutat3;
+		ciutat[3]=ciutat4;
+		ciutat[4]=ciutat5;
+		ciutat[5]=ciutat6;
+		
 		// Convertir a Arraylist
 
 		ArrayList<String> arrayCiutats = new ArrayList();
