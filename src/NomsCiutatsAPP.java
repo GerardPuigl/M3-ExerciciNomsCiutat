64d65d6d556ca301d024b03e56e9ecd3833169f8
gerardpuig <gerardpuig@192.168.1.131>
2021-01-18 16:09:46 +0100

    Fase3 Completada

--- src/NomsCiutatsAPP.java
+++ src/NomsCiutatsAPP.java
@@ -3,38 +3,46 @@ import java.util.*;
 public class NomsCiutatsAPP {
 	public static void main(String[] arg) {
 
-		// Crear 6 variables String y pedir por consolsa introducir nombres
+		Scanner sc = new Scanner(System.in);
 
-		String[] ciutat = new String[6];
+		// Crear 6 variables String y pedir por consolsa introducir nombres ciudades
+
 		System.out.println("Introdueix el nom de 6 ciutats:");
 
-		for (int i = 0; i < 6; i++) {
-			Scanner sc = new Scanner(System.in);
-			ciutat[i] = sc.next();
-		}
+		String ciutat1 = sc.next();
+		String ciutat2 = sc.next();
+		String ciutat3 = sc.next();
+		String ciutat4 = sc.next();
+		String ciutat5 = sc.next();
+		String ciutat6 = sc.next();
 
 		// Convertir a Arraylist
 
-		ArrayList<String> arrayCiutats = new ArrayList();
+		String[] arrayCiutats = new String[6];
 
-		for (String s : ciutat) {
-			arrayCiutats.add(s);
-		}
-		
-	
-		//Cambiar las 'a' por '4'
-		ArrayList<String> arrayCiutatsModificada = new ArrayList();
-		
-		for (String s:arrayCiutats) {
-			s=s.replace('a', '4');
-			arrayCiutatsModificada.add(s);
+		arrayCiutats[0] = ciutat1;
+		arrayCiutats[1] = ciutat2;
+		arrayCiutats[2] = ciutat3;
+		arrayCiutats[3] = ciutat4;
+		arrayCiutats[4] = ciutat5;
+		arrayCiutats[5] = ciutat6;
+
+		Arrays.sort(arrayCiutats);
+
+		// Cambiar las 'a' por '4'
+
+		String[] arrayCiutatsModificades = new String[6];
+
+		for (int i = 0; i < arrayCiutats.length; i++) {
+			arrayCiutatsModificades[i] = arrayCiutats[i].replace('a', '4');
 		}
 
 		// Ordenar Array
-		Collections.sort(arrayCiutatsModificada);
-		
-		System.out.println(arrayCiutatsModificada);
-		
+		Arrays.sort(arrayCiutatsModificades);
+
+		System.out.println(Arrays.toString(arrayCiutats));
+		System.out.println(Arrays.toString(arrayCiutatsModificades));
+
 	}
 
 }
